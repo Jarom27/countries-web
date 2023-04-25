ac377d934974dc0817001654f31222a059cedf76
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-04-24 18:34:38 -0700

    Ultimo commit

--- src/components/view-details/BorderTag.jsx
+++ src/components/view-details/BorderTag.jsx
@@ -5,7 +5,7 @@ export default function BorderTag({country_name}) {
   const {theme} = useContext(ThemeContext)
   return (
     <>
-      <span className={theme == 'light'? '  light-mode-lighter border-tag me-3 px-2': '  dark-mode-lighter border-tag me-3 px-2'}>{country_name}</span>
+      <span className={theme == 'light'? '  light-mode-lighter border-tag me-3 px-2 mb-1': '  dark-mode-lighter border-tag me-3 px-2 mb-1'}>{country_name}</span>
     </>
     
   )
